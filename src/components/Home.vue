1d0a9e14d37eb1224323405b782d2a0423bfc6d6
iwatos <nonbiriactive@gmail.com>
2019-02-27 22:17:48 +0900

    add:googleログイン機能の実装

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="home">
     <h1>{{ msg }}</h1>
-    <button>Googleアカウントでログイン</button>
+    <button @click="googleLogin">Googleアカウントでログイン</button>
   </div>
 </template>
 
@@ -12,7 +12,12 @@ export default {
     return {
       msg: "Welcome to MyMarkdown"
     }
-  }
+  },
+  methods: {
+    googleLogin: function() {
+      firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+    }
+  },
 }
 </script>
 
